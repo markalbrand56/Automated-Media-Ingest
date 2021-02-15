3943aa8c67194c843bdc54c06824a7c51ae089ba
Mark Albrand <mark.albrand5@gmail.com>
2021-02-14 20:50:46 -0600

    Basic structure for the proyect

--- Main.go
+++ Main.go
@@ -5,7 +5,24 @@ import (
 	"time"
 )
 
+func path() string { /*Path of the SD card itself*/
+	sdPath := "F:" /*Temporary*/
+	return sdPath
+}
+
+func destiny(folderName string, localPath string) string { /*Path to the new folder in the device*/
+	destinyPath := localPath + folderName
+	return destinyPath
+}
+
+func searchMedia(mediaTypes string, pathFolder string) { /*Search for all media in the SD card*/
+	/*Find all media to ingest*/
+}
+
 func main() {
+	dataTypes := [4]string{".mp4", ".arw", ".jpg"}
+
 	currentTime := time.Now().Format("2006-01-02")
 	fmt.Printf("%s\n", currentTime)
+	fmt.Println(dataTypes)
 }
